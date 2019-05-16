76117fa5997ae3cc5ef1ee628590665e1dbb513d
Joey Driscoll <driscoll.recording@gmail.com>
2019-05-16 10:25:15 -0500

    initial commit

--- src/scripts/dbCalls.js
+++ src/scripts/dbCalls.js
@@ -1,9 +1,44 @@
 const baseURL = "http://0.0.0.0:8088";
 
 const APIManager = {
-    getAllPlaces: function() {
-        return fetch(`${baseURL}/places`)
+    getSignedIn: (resource) => {
+        return fetch(`${baseURL}/${resource}`)
+            .then(result => result.json())
+    },
+    getAllInterests: function() {
+        return fetch(`${baseURL}/interests?_expand=place`)
                 .then(response => response.json())
+    },
+    getAllReviews: function(interestId) {
+        return fetch(`${baseURL}/reviews?interestId=${interestId}`)
+                .then(response => response.json())
+    },
+    deleteInterest: (resource, id) => {
+        return fetch(`${baseURL}/${resource}/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    },
+    postPlace: (resource, data) => {
+        return fetch(`${baseURL}/${resource}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+    },
+    putPlace: (resource, id, data) => {
+        return fetch(`${baseURL}/${resource}/${id}`, {
+            method: "PUT",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
     }
 }
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,63 @@
-import APIManager from "./dbCalls";
+import createUser from "./signUp";
+import interestsPrinter from "./interestBuilder"
+import signIn from "./signIn";
+import checkforUser from "./welcomeHandler";
+import addReview from "./addReview"
+import addInterest from "./addInterests"
+checkforUser()
+interestsPrinter()
 
-//testing import of dbcalls and calling getAllPlaces
-APIManager.getAllPlaces()
-    .then((places) => {
-        console.log("Oh the places you will go:", places);
-    });
+
+
+//============== overlay btn open/close ===========================
+document.querySelector("#btn_close_welcome").addEventListener("click", event => {
+    document.getElementById("overlay_welcome").style.display = "none"
+})
+document.querySelector("#btn_close_signup").addEventListener("click", event => {
+    document.getElementById("overlay_signup").style.display = "none"
+})
+document.querySelector("#btn_close_signin").addEventListener("click", event => {
+    document.getElementById("overlay_signin").style.display = "none"
+})
+
+document.querySelector("#btn_open_signup_1").addEventListener("click", event => {
+    document.getElementById("overlay_signup").style.display = "block"
+    document.getElementById("overlay_welcome").style.display = "none"
+
+})
+document.querySelector("#btn_open_signin_1").addEventListener("click", event => {
+    document.getElementById("overlay_signin").style.display = "block"
+    document.getElementById("overlay_welcome").style.display = "none"
+
+})
+
+document.querySelector("#btn_signout").addEventListener("click", event => {
+    sessionStorage.removeItem("user")
+    document.getElementById("overlay_welcome").style.display = "block"
+})
+
+// =============== register new user btn ==========================
+document.getElementById("btn_register").addEventListener("click", event => {
+    event.preventDefault();
+    createUser();
+})
+// ============= signIn =====================================================
+
+document.getElementById("btn_sign_in").addEventListener("click", event => {
+    event.preventDefault();
+    signIn();
+})
+/// add interests and reviews
+document.getElementById("btn_add_interest").addEventListener("click", event => {
+    event.preventDefault()
+    addInterest()
+    document.location.reload()
+})
+
+document.getElementById("btn_add_review").addEventListener("click", event => {
+    event.preventDefault()
+    // const displayReview = document.querySelector(".add_review")
+    // displayReview.setAttribute("id","unhide")
+    addReview()
+    document.location.reload()
+})
